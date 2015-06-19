c45f6bce3114a3f68a0fb3c24ae5da4e8acfd7ec
Sam Joch <samuel@guiora.com>
2015-06-19 13:12:48 +0200

    Add cancel, scheduledAt and rule

---
+++ joby-meteor-tests.js
@@ -0,0 +1,34 @@
+
+if (Meteor.isClient) {
+  Tinytest.add('Joby does not exists in client side', function (test) {
+    test.equal(typeof Joby, 'undefined');
+  });
+  return;
+}
+
+var queue = new Joby.Queue('queue');
+
+console.log(Meteor.isServer ? 'server' : 'client')
+
+Tinytest.add('queue has a name', function (test) {
+  test.equal(queue.name, 'queue');
+});
+
+Tinytest.add('queue has a collection', function(test) {
+  test.equal(queue.collection, Joby.Jobs);
+});
+
+Tinytest.add('queue can enqueue a task', function(test) {
+  jobId = queue.enqueue('stuff', { foo: 'bar' });
+  job = Joby.Jobs.findOne(jobId);
+  test.equal(job.task, 'stuff');
+  test.equal(job.status, 'queued');
+});
+
+Tinytest.add('queue can cancel a job', function(test) {
+  jobId = queue.enqueue('stuff', { foo: 'bar' });
+  job = Joby.Jobs.findOne(jobId);
+  job = queue.cancel(job);
+  test.equal(job.status, 'cancelled');
+});
+
--- joby-meteor.js
+++ joby-meteor.js
@@ -1,5 +1,14 @@
 
+if (Meteor.isClient) {
+  return;
+}
+
 var Joby = this.Joby = {};
+
+var STATUS = {
+  QUEUED: 'queued',
+  CANCELLED: 'cancelled'
+};
 
 Joby.Jobs = new Mongo.Collection('jobs');
 
@@ -9,18 +18,35 @@ Joby.Queue = function Queue(name, options) {
   this.collection = Joby.Jobs;
 };
 
-Joby.Queue.prototype.enqueue = function(name, params, options, cb) {
-  if (!cb && typeof options === 'function') {
-    cb = options;
-    options = {};
-  }
+Joby.Queue.prototype.enqueue = function(task, params, opts) {
+  opts = opts || {};
+  opts.interval = opts.interval || 500;
+  var initialStatus = {
+    at: new Date(),
+    by: opts.by || 'unknow',
+    label: STATUS.QUEUED
+  };
   var job = {
-    status: 'queued',
-    name: name,
+    createdAt: new Date(),
+    updatedAt: initialStatus.at,
+    scheduledAt: opts.scheduledAt || new Date(+new Date() + opts.interval),
+    rule: opts.rule,
+    task: task,
     queue: this.name,
     params: params,
-    enqueuedAt: new Date()
+    status: initialStatus.label,
+    statuses: [initialStatus]
   }
   return this.collection.insert(job);
+};
+
+Joby.Queue.prototype.cancel = function(job, opts) {
+  opts = opts || {};
+  var newStatus = { label: STATUS.CANCELLED, at: new Date(), by: opts.by }
+  this.collection.update({ _id: job._id }, {
+    $set: { status: newStatus.label, updatedAt: newStatus.at },
+    $addToSet: { statuses: newStatus }
+  });
+  return this.collection.findOne(job._id);
 };
 
